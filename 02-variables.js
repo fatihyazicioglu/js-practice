00f96f256558924c3c11174978fba7e226b6bd0b
fatihyazicioglu <f.yzcgl1@gmail.com>
2022-03-05 15:17:31 +0100

    updated

--- 02-variables.js
+++ 02-variables.js
@@ -3,8 +3,14 @@ console.log ("kodluyoruz.org")
 serverName = "https://kodluyoruz.org"
 console.log(serverName)
 
-let password = "1234"
+let password = "1234";
 console.log(password)
 
-let fullName = "fatih"
+let fullName = "fatih";
 console.log(fullName)
+
+fullName = "lorem2"
+console.log(fullName)
+
+fullName + "new added info"
+console.log(fullName + " test info")

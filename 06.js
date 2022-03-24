6c5638ca9b8ec7c7cd2ad3772ba591887a9b0bd0
fatihyazicioglu <f.yzcgl1@gmail.com>
2022-03-24 22:52:45 +0100

    updated

---
+++ 06.js
@@ -0,0 +1,16 @@
+let number2 = "11px"
+number2 = parseInt(number2)
+console.log( "number2: ", number2, typeof(number2));
+
+
+let number4 = "11px"
+number4 = parseFloat(number4)
+console.log( "number5: ", number4, typeof(number4));
+
+
+
+
+
+let number5 = 55
+number5 = number5.toString()
+console.log( number5, typeof(number5));

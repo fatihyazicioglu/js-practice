cda3e78dc02d72dee66342f37ae28f78b64b495f
fatihyazicioglu <f.yzcgl1@gmail.com>
2022-03-24 23:01:40 +0100

    updated

--- 06.js
+++ 06.js
@@ -3,8 +3,8 @@ number2 = parseInt(number2)
 console.log( "number2: ", number2, typeof(number2));
 
 
-let number4 = "11px"
-number4 = parseFloat(number4)
+let number4 = "11.1px"
+number4 = parseInt(number4)
 console.log( "number5: ", number4, typeof(number4));
 
 
---
+++ 07.js
@@ -0,0 +1,5 @@
+let username="fatih"
+const DOMAIN = "12313.org"
+
+let email = username + "@"+ DOMAIN
+console.log("Merhaba", username, "welcome to our site", "your mail:",email);

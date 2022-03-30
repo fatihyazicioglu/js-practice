353638eff08665a0f51190e463d0bf1650eae87a
fatihyazicioglu <f.yzcgl1@gmail.com>
2022-03-30 21:22:48 +0200

    updated

---
+++ for-loops.js
@@ -0,0 +1,18 @@
+let users = ["lorem", "Ipsum","Dolor"]
+
+
+for (let i = 0; i<10 ; i++){
+    console.log(i);
+}
+
+let index = 0
+for (; index <users.length; index++) {
+    console.log(users[index]);
+}
+
+
+let counter = 0
+for(; counter<10; counter++{
+    if (counter ===5){continue}
+    console.log(counter);
+}) 

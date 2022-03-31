482f0d7927fed7d988e1fbf5fb5d15faab7fab0b
fatihyazicioglu <f.yzcgl1@gmail.com>
2022-03-31 23:53:22 +0200

    updated

--- ternary-logical-operators.js
+++ ternary-logical-operators.js
@@ -1,3 +1,26 @@
-let price = "100"
-console.log(price = 1);
+let price = "5"
+console.log(price = 66);
 console.log(price == 100);
+
+console.log(price ===100);
+
+console.log(price !=5);
+
+
+
+let username =  ("enter you username")
+
+if (username.length>0){
+    console.log(`"username" ,${username}`);
+}else{
+    console.log("no information");
+}
+
+let info = document.querySelector("#info")
+if(userName && age >=18){
+    info.innerHTML = "you can take driver license"
+}else if (!userName){
+    info.innerHTML = "there is no username"
+}else if (!(age>=18)){
+    info.innerHTML = "no age info or bigger than 18"
+}

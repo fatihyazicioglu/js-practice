6c1e352e89bccbbfd021e751054cf2c5429e29c9
fatihyazicioglu <f.yzcgl1@gmail.com>
2022-04-04 03:00:17 +0200

    updated

---
+++ function.js
@@ -0,0 +1,16 @@
+function hello (){
+    console.log("merhaba");
+    bunuyazinca()
+}
+hello()
+hello()
+hello()
+hello()
+hello()
+
+
+function bunuyazinca() {
+    console.log("bu cikacak");
+}
+
+bunuyazinca()

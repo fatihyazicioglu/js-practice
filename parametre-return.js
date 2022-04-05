69d048c790274b5712ea08b96f9d9cefc22a1062
fatihyazicioglu <f.yzcgl1@gmail.com>
2022-04-05 15:15:12 +0200

    js practice

--- parametre-return.js
+++ parametre-return.js
@@ -8,3 +8,14 @@ greetings()
 
 let info = greetings ("parametre")
 console.log(info);
+
+greetings ("Parametre")
+
+function greeting2 (firstName, lastName){
+    let info = `${firstName} ${lastName}`
+    return info
+}
+function greeting2 (firstName, lastName){
+greeting2 ("Ad", "Soyad")
+let info = "deneme" //???????
+console.log(info);

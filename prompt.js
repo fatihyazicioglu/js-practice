b1d2e18fde54596a563a6318839ec1b1a71bef1c
fatihyazicioglu <f.yzcgl1@gmail.com>
2022-04-12 16:08:25 +0200

    updated

--- prompt.js
+++ prompt.js
@@ -1,2 +1,13 @@
-let fullName = prompt("please enter your name")
-console.log(fullName);
+// let fullName = prompt("please enter your name")
+// console.log(fullName);,
+
+let domain = "kodluyoruz"
+let isActive = false
+let items = [1,2,3,isActive,domain]
+console.log(items);
+
+items+=[1,2,3,]
+console.log(items);
+
+let emptyArray = []
+console.log(emptyArray.length);

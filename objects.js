fba8688f78867e2cbae5425654fd266ea4bc1689
fatihyazicioglu <f.yzcgl1@gmail.com>
2022-04-25 00:13:22 +0200

    object destructuring

---
+++ objects.js
@@ -0,0 +1,26 @@
+let user1={
+    firstName : "Bilgin",
+    lastName : "Uzman",
+    score:[1,2,3,4],
+    isActive : true,
+    shortName:function(){
+        return `${this.firstName[0].toUpperCase()}.${this.lastName}`
+    }
+};
+console.log();
+
+//Object Destructuring:
+let settings = {
+    userName: "loremIpsum",
+    password: "Badpassword",
+    isActive: false,
+    ip :"127.0.0.1",
+    serverName :"kodluyoruz.org",
+}
+
+let userName = settings.userName
+console.log(userName);
+let {userName :user ,password,isActive,ip:serverIp,serverName} = settings
+console.log(settings);
+console.log(user);
+console.log(serverIp);

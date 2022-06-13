ab9652f14b572de8a8edd5e814b9145b755c72ee
fatihyazicioglu <f.yzcgl1@gmail.com>
2022-06-13 21:06:43 +0200

    map

---
+++ localStorage.js
@@ -0,0 +1,10 @@
+const USERS = ["AYSE", "MehMet","TugCe", "AkSEL"]
+const NEW_USERS = USERS.map(user=>user.toLowerCase())
+console.log(NEW_USERS);
+
+
+const USERS_OBJ = USERS.map(item =>{
+
+
+    return { }
+})
